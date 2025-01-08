f509ba9c84a5663edb7825d65e7e52c3ecbc95f3
AS121004 <128366253+AS121004@users.noreply.github.com>
2025-01-09 00:34:38 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './Pages/Home.jsx';
+import Adduser from './Pages/Adduser';
+import Viewuser from './Pages/Viewuser';
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/adduser' element={<Adduser />} />
+        <Route path='/viewuser/:id' element={<Viewuser />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
